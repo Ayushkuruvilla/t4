4b77883ec6e17c21d0e274c76002275c2e87ea24
Ayush Kuruvilla <ayush.kuruvilla@gmail.com>
2021-10-18 00:53:51 +0530

    added files

--- fash/main/urls.py
+++ fash/main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns=[
 path("", views.index, name="index"),
+path("pred", views.pred, name="pred"),
+path("tags", views.tags, name="tags"),
 path("v1/", views.v1, name="view 1"),
+path("pyfun",views.pyfun)
 ]
--- fash/main/views.py
+++ fash/main/views.py
@@ -1,16 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.urls import reverse
 from .forms import ContactForm, SnippetForm
 # Create your views here.
 
 def index(request):
     return render(None,'ind.html')
 
+def pyfun(request):
+    print("Hello")
+    return render(request,"pred.html",{'major':['symmetrical skirt',
+ 'regular (fit) bag, wallet',
+ 'symmetrical jacket',
+ 'zip-up bag, wallet'],'minor':['collarless collar',
+ 'collarless neckline',
+ 'v-neck neckline',
+ 'wrist-length sleeve']})
+    
+def pred(request):
+    if request.method=="POST":
+        print("Im here")
+        print(request.POST)
+        return render(request,"tags.html")
+    return render(None,'pred.html')
+
+def tags(request):
+    if request.method=="POST":
+        print("Im here")
+        print(request.POST)
+    return render(None,'tags.html')    
+
+
 def v1(request):
     return HttpResponse("<h1>Hello</h1>")
-
-
-
 
 
 def contact(request):
